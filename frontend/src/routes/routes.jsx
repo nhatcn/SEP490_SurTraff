01cf75177ce49b590ab6b01d68f011477f13da6e
ThongNg463 <155498816+ThongNg463@users.noreply.github.com>
2025-06-14 01:29:56 +0700

    Update routes (#19)
    Update routes

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -12,11 +12,15 @@ import ForgotPasswordPage from '../Pages/Auth/ForgotPasswordPage';
 import UserDashboard from '../Pages/Dashboard/User/UserDashboard';
 import AddCameraDashboard from '../Pages/Dashboard/Camera/AddCamera';
 import AccidentDashboard from '../Pages/Dashboard/Accident/AccidentDashboard';
+import AccidentStatistics from '../Pages/Dashboard/AccidentStatistics';
 import ViolationList from '../Pages/Violations/ViolationList';
 import ViolationDetail from '../Pages/Violations/ViolationDetail';
 import ViolationHistory from '../Pages/Violations/ViolationHistory';
+import ViolationStatistics from '../Pages/Dashboard/ViolationStatistics';
 import EditCamera from '../Pages/Dashboard/Camera/EditCamera';
 import UserProfileDashboard from '../Pages/Dashboard/User/UserProfileDashboard';
+import VehicleDashboard from '../Pages/Dashboard/Vehicle/VehicleDashboard';
+import VehicleDetails from '../Pages/Dashboard/Vehicle/VehicleDetails';
 
 
 const RoutesConfig = () => {
@@ -36,6 +40,11 @@ const RoutesConfig = () => {
       <Route path="/violations/:id" element={<ViolationDetail />} />
       <Route path="/violations/history/:plate" element={<ViolationHistory />} />
       <Route path="/profile" element={<UserProfileDashboard />} />
+      <Route path="/accidentstatistics" element={<AccidentStatistics />} />
+      <Route path="/violationstatistics" element={<ViolationStatistics />} />
+      <Route path="/vehicledetails" element={<Vehicledetails/>} />
+      <Route path="/vehicles" element={<VehicleDashboard />} />
+      <Route path="/vehicles/:id" element={<VehicleDetails />} />
     </Routes>
   );
 };
