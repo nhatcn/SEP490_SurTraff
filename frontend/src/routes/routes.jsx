7cdb2f0c1d36eb1d166f9f473d256679e9732314
nhatcn <ngminhat24@gmail.com>
2025-07-09 22:57:51 +0700

    modify UI camera, home, profile, tracks

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -25,6 +25,7 @@ import VehicleDetail from '../Pages/Dashboard/Vehicle/VehicleDetails';
 import CustomerHome from '../Pages/Home/HomePage';
 import CustomerProfile from '../Pages/Dashboard/User/CustomerProfile';
 import LoginPage from '../Pages/Auth/LoginPage';
+import VehicleTrackingDashboard from '../Pages/Dashboard/VehicleTraking/VehicleTrackingDashboard';
 
 
 
@@ -40,6 +41,7 @@ const RoutesConfig = () => {
       <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
       <Route path="/userdashboard" element={<UserDashboard />} />
       <Route path="/addcamera" element={<AddCamera />} />
+       <Route path="/tracks" element={<VehicleTrackingDashboard />} />
       <Route path="/accidentdashboard" element={<AccidentDashboard />} />
       <Route path="/cameras/edit/:id" element={<EditCamera />} />
       <Route path="/violations" element={<ViolationList />} />
