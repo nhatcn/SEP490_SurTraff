a80188dfb34df97fab7fbb4a813675c0bd47a776
nhatcn <ngminhat24@gmail.com>
2025-07-29 18:06:09 +0700

    fix user

--- backend/api/v1/endpoints/camera.py
+++ backend/api/v1/endpoints/camera.py
@@ -9,6 +9,7 @@ from services.camera.camera_service import (
     stream_violation_video_service,
     stream_violation_video_service1,
     stream_count_video_service,
+    # stream_accident_video_service,
     stream_plate_with_ocr_video_service,
     stream_violation_wrongway_video_service
 )
@@ -23,6 +24,9 @@ from services.stream_overspeed_service import (
 from services.nohelmet_service import (
     stream_no_helmet_service
 )
+# from services.camera.wrongwayService import (
+#     stream_violation_wrongway_video_service
+# )
 
 from services.traffic_density_service import analyze_traffic_video
 from services.pothole_detection_service import detect_potholes_in_video
@@ -32,10 +36,9 @@ from services.camera.red_light_violation_service import (
 )
 from db.session import get_db
 from models.model import Camera
-from schemas.camera_schema import CameraCreate, CameraUpdate
-
+from schemas.camera_schema  import CameraCreate, CameraUpdate
+from fastapi.responses import JSONResponse
 router = APIRouter()
-
 
 @router.post("/thumbnail/extract")
 def extract_thumbnail(stream_url: str = Body(..., embed=True)):
@@ -44,13 +47,10 @@ def extract_thumbnail(stream_url: str = Body(..., embed=True)):
         return Response(content=image_bytes, media_type="image/jpeg")
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
-
-
 @router.get("/cameras")
 def get_all_cameras(db: Session = Depends(get_db)):
     cameras = db.query(Camera).all()
     return jsonable_encoder(cameras)
-
 
 @router.get("/cameras/{camera_id}")
 def get_camera_by_id(camera_id: int, db: Session = Depends(get_db)):
@@ -59,13 +59,12 @@ def get_camera_by_id(camera_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Camera not found")
     return jsonable_encoder(camera)
 
-
 @router.post("/cameras")
 def create_camera(camera: CameraCreate, db: Session = Depends(get_db)):
     db_camera = db.query(Camera).filter(Camera.ip_address == camera.ip_address).first()
     if db_camera:
         raise HTTPException(status_code=400, detail="Camera with this IP already exists")
-
+    
     new_camera = Camera(
         name=camera.name,
         ip_address=camera.ip_address,
@@ -82,7 +81,7 @@ def update_camera(camera_id: int, camera: CameraUpdate, db: Session = Depends(ge
     db_camera = db.query(Camera).filter(Camera.id == camera_id).first()
     if not db_camera:
         raise HTTPException(status_code=404, detail="Camera not found")
-
+    
     db_camera.name = camera.name
     db_camera.ip_address = camera.ip_address
     db_camera.stream_url = camera.stream_url
@@ -97,11 +96,10 @@ def delete_camera(camera_id: int, db: Session = Depends(get_db)):
     db_camera = db.query(Camera).filter(Camera.id == camera_id).first()
     if not db_camera:
         raise HTTPException(status_code=404, detail="Camera not found")
-
+    
     db.delete(db_camera)
     db.commit()
     return {"detail": "Camera deleted successfully"}
-
 
 @router.get("/video/{camera_id}")
 def stream_video(camera_id: int, db: Session = Depends(get_db)):
@@ -109,12 +107,7 @@ def stream_video(camera_id: int, db: Session = Depends(get_db)):
     if not camera:
         raise HTTPException(status_code=404, detail="Camera not found")
 
-    if camera_id == 1:
-        return StreamingResponse(
-            stream_violation_wrongway_video_service(camera.stream_url, camera.id, db),
-            media_type="multipart/x-mixed-replace; boundary=frame"
-        )
-    elif camera_id == 2:
+    if camera_id == 2:
         return StreamingResponse(
             stream_violation_video_service(camera.stream_url, camera.id),
             media_type="multipart/x-mixed-replace; boundary=frame"
@@ -139,9 +132,9 @@ def stream_video(camera_id: int, db: Session = Depends(get_db)):
             analyze_traffic_video(camera.stream_url, camera.id),
             media_type="multipart/x-mixed-replace; boundary=frame"
         )
-    elif camera_id == 12:
+    elif camera_id == 7:
         return StreamingResponse(
-            stream_no_helmet_service(camera.stream_url, camera.id),
+            detect_potholes_in_video(camera.stream_url, camera.id, db),
             media_type="multipart/x-mixed-replace; boundary=frame"
         )
     elif camera_id == 8:
@@ -149,16 +142,16 @@ def stream_video(camera_id: int, db: Session = Depends(get_db)):
             stream_violation_wrongway_video_service(camera.stream_url, camera.id, db),
             media_type="multipart/x-mixed-replace; boundary=frame"
         )
+    elif camera_id == 9:
+        return StreamingResponse(
+            stream_overspeed_service(camera.stream_url, camera.id, db),
+            media_type="multipart/x-mixed-replace; boundary=frame"
+        )
     elif camera_id == 11:
         return StreamingResponse(
-            analyze_traffic_video(camera.stream_url, camera.id, db),
+            stream_no_helmet_service(camera.stream_url, camera.id),
             media_type="multipart/x-mixed-replace; boundary=frame"
-        )
-    elif camera_id == 13:
-        return StreamingResponse(
-            detect_potholes_in_video(camera.stream_url, camera.id, db),
-            media_type="multipart/x-mixed-replace; boundary=frame"
-        )
+        )        
     elif camera_id >= 25:
         return StreamingResponse(
             stream_violation_video_service1(camera.stream_url, camera.id),
@@ -169,3 +162,4 @@ def stream_video(camera_id: int, db: Session = Depends(get_db)):
             analyze_traffic_video(camera.stream_url, camera.id),
             media_type="multipart/x-mixed-replace; boundary=frame"
         )
+        
