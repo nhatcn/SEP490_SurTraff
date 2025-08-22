01ae6070e312e95551d619310bcbfb72db619b00
Anh Tu Nguyen <tunace172085@gmail.com>
2025-08-22 12:06:02 +0700

    update by AnhTu

--- frontend/src/components/Link/LinkAPI.jsx
+++ frontend/src/components/Link/LinkAPI.jsx
@@ -1,3 +1,3 @@
-const API_URL_BE = "http://localhost:8081/";
+const API_URL_BE = process.env.REACT_APP_API_URL_BE;
 const API_URL_FAST = "http://localhost:8000/"; 
 export { API_URL_BE, API_URL_FAST }
