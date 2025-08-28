335e9bb8799e6fe508fd0345ce34fdfd3dc6ad0b
nhatcn <149814696+nhatcn@users.noreply.github.com>
2025-08-29 04:17:41 +0700

    Update LinkAPI.jsx

--- frontend/src/components/Link/LinkAPI.jsx
+++ frontend/src/components/Link/LinkAPI.jsx
@@ -1,3 +1,3 @@
-const API_URL_BE = "http://localhost:8081/";
-const API_URL_FAST = "http://localhost:8000/"; 
+const API_URL_BE = "https://surtraff.onrender.com/";
+const API_URL_FAST = "https://starling-touching-hare.ngrok-free.app/"; 
 export { API_URL_BE, API_URL_FAST }
